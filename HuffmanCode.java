1c8bf6655cdad2f43029a16e93a05c063c8dc30b
tresl <trenton_lam@yahoo.com>
2015-06-29 09:30:02 -0700

    A later version of same code

--- HuffmanCode.java
+++ HuffmanCode.java
@@ -1,67 +1,102 @@
+/*
+Trenton Lam
+CSE 143
+Carl Ross
+June 4th 2015
+
+Can compress and decompress text files using huffman compression
+*/
+
 import java.util.*;
 import java.io.*;
-
-public class HuffmanCode {
-   private HuffmanNode root;
-
+public class HuffmanCode{
+   
+   private HuffmanNode huffmanRoot; //The top of the huffman code
+   
+   //Pre: Assumes that frequencies is not null, in the format of 
+   //     index = character, frequencies[i] = number of occurences 
+   //Post: Initializes a huffman code object
    public HuffmanCode(int[] frequencies) {
-      Queue<HuffmanNode> branches = new PriorityQueue<HuffmanNode>();
-      for(int i = 0; i < frequencies.length; i++) {
-         if (frequencies[i] != 0) {
-            branches.add(new HuffmanNode((char)('0' + i), frequencies[i])); 
+      Queue<HuffmanNode> order = new PriorityQueue<HuffmanNode>();
+      for(int i = 0; i < frequencies.length; i++){
+         if(frequencies[i] != 0){ 
+            order.add(new HuffmanNode((char) ('0' + (i - 48)) , frequencies[i]));
          }
       }
-      while (branches.size() > 1) {
-         HuffmanNode node1 = branches.remove();
-         HuffmanNode node2 = branches.remove();
-         HuffmanNode node = new HuffmanNode(null, node1.frequency + node2.frequency, node1, node2);
-         branches.add(node);
+      while(order.size() > 1){ //Empyting out the queue and making the tree
+         HuffmanNode left = order.remove();
+         HuffmanNode right = order.remove();
+         HuffmanNode currentRoot = new HuffmanNode(null ,left.freq + right.freq, left, right);
+         order.add(currentRoot);
+      }      
+      this.huffmanRoot = order.remove();
+   }
+   
+   //Pre: Assumes scanner is not null, accepts data in the format of 
+   //     character -> frequency.
+   //Post: Initializes a huffmancode object using an existing
+   public HuffmanCode(Scanner input) {
+      while(input.hasNextLine()){
+         int charName = Integer.parseInt(input.nextLine());
+         String code = input.nextLine();
+         this.huffmanRoot = this.createTree(huffmanRoot, code, charName);
       }
-      this.root = branches.remove();       
    }
    
-   /*public HuffmanCode(Scanner input) {
-      readTreeHelper(input, this.root);
-   }
-   
-   private HuffmanNode readTreeHelper(Scanner input, HuffmanNode node) {
-      while (input.hasNextLine()) {
-         int n = Integer.parseInt(input.nextLine());
-         String binary = input.nextLine();
-      }
-   }*/
-   
-   public void save(PrintStream output) {
-      this.save(output, this.root, "");
-   }
-   
-   private void save(PrintStream output, HuffmanNode current, String acc) {
-      if (current != null) {  
-         if (current.left != null && current.right != null) {
-            output.println(current.letter - '0');
-            output.println(acc);
+
+   //Post: Creates a huffman code that navigates through ones (left) and zeroes(right)
+   private HuffmanNode createTree(HuffmanNode root, String code, int charName) {
+      if(code.equals("")){
+         root = new HuffmanNode((char)charName, 0);
+      } else{
+         if(root == null){
+            root = new HuffmanNode(null, 0);   
+         }   
+         if(code.charAt(0) == '0'){
+            root.left = createTree(root.left, code.substring(1,code.length()), charName);
          } else {
-            save(output, current.left, acc + "0");
-            save(output, current.right, acc + "1");
+            root.right = createTree(root.right, code.substring(1, code.length()), charName);
          }
-      }   
-   }
-   
-   public void translate(BitInputStream input, PrintStream output) {
-      this.translate(input, output, this.root);
-   }
-   
-   private void translate(BitInputStream input, PrintStream output, HuffmanNode current) {
-      if (current.letter != null) {
-         output.write((char) ('0' + current.letter));
-         current = this.root;
       }
-      if (input.hasNextBit()) {
-         if (input.nextBit() == 0) {
-            translate(input, output, current.left);
+      return root;
+   }
+  
+  //Pre: Assumes output is not null
+  //Post: Saves the huffman code to an output. It creates a pathway
+  //      using 0 as left and 1 as right.
+   public void save(PrintStream output){
+      this.save(huffmanRoot,"", output);
+   }
+   
+   //Post: Traverses the tree saving both the path and the character
+   private void save(HuffmanNode root,String path, PrintStream output){
+   if(root != null){
+         if(root.data != null){
+            output.println((int)(0 + root.data));
+            output.println(path);
          } else {
-            translate(input, output, current.right);
+            this.save(root.left,path + "0", output);
+            this.save(root.right, path + "1", output);
          }
       }
    }
+   
+   
+   //Pre: Requires that both input and output are not null
+   //Post: Decodes the huffmancode given
+   public void translate(BitInputStream input, PrintStream output){
+   HuffmanNode currentNode = this.huffmanRoot;
+      while(input.hasNextBit()){
+         if(currentNode.data != null){
+            output.write(currentNode.data);
+            currentNode = this.huffmanRoot;
+         }
+         if(input.nextBit() == 0){
+            currentNode = currentNode.left;
+         } else {
+            currentNode = currentNode.right;
+         }
+      }
+      output.write(currentNode.data); //Prints the last node's data
+    }
 }
